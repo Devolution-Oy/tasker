4bdc13953be80b6b450955864ea39b821353f866
Mikko Kurkela <mikko.kurkela@devolution.fi>
2020-05-30 16:27:29 +0300

    Fix linter

--- diff.js
+++ diff.js
@@ -1,18 +1,18 @@
 /**
- * 
- * @param {import('probot').Context} context 
+ *
+ * @param {import('probot').Context} context
  */
 const getCommit = async context => {
   context.log('Trying to get diff');
-  context.log(context.event)
+  context.log(context.event);
   return context.github.repos.getCommit(context.repo({
     method: 'GET',
     ref: context.payload.head_commit.id,
-    headers: { Accept: 'application/vnd.github.diff' },
+    headers: { Accept: 'application/vnd.github.diff' }
   }));
-}
+};
 
 module.exports = async context => {
   const commit = await getCommit(context);
-  return diff = commit.data
-}
+  return commit.data;
+};
