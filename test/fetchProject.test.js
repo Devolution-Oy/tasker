c7cd28c1a73aa3e09179d5e5b4dc3dba0f79fc63
Mikko Kurkela <mikko.kurkela@devolution.fi>
2020-07-21 14:23:59 +0300

    Fix semistandard

--- test/fetchProject.test.js
+++ test/fetchProject.test.js
@@ -1,17 +1,16 @@
 const nock = require('nock');
 const fetchProject = require('../utils/fetchProject');
 
-
 describe('FethcProject', () => {
   beforeEach(() => {
     nock.disableNetConnect();
   });
   it('Send http getProject request with repository name', async () => {
     nock(process.env.ROSTER_URL, {
-        reqHeaders: {
-          Authorization: headerValue => headerValue.includes('')
-        }
-      }).post('/getProject',req => {
+      reqHeaders: {
+        Authorization: headerValue => headerValue.includes('')
+      }
+    }).post('/getProject', req => {
       expect(req.project).toBe('project1');
       return true;
     }).reply(200);
--- utils/fetchProject.js
+++ utils/fetchProject.js
@@ -12,4 +12,4 @@ module.exports = async (project) => {
     {
       headers: headers
     });
-}
+};
