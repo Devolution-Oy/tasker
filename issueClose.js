c1659d07e5fc6dfbb7c33dc6d44d28a7ee326773
Mikko Kurkela <mikko.kurkela@devolution.fi>
2020-07-21 16:25:44 +0300

    Get issue closing price from project configs

--- issueClose.js
+++ issueClose.js
@@ -1,6 +1,6 @@
 const axios = require('axios');
+const getPrice = require('./utils/getPrice');
 
-// TODO: Call fetchProject function to get project issue prices
 const sendPayment = async (amount, issue, project) => {
   const headers = {
     authorization: process.env.TASKER_APP_ID
@@ -44,15 +44,15 @@ module.exports = async context => {
   }
 
   const repo = context.payload.repository.name;
-  return sendPayment(50, issue, repo)
-    .then(res => {
-      context.log('Sent');
-      const issueComment = context.issue({ body: `Sent payment to ${issue.assignee.login}` });
-      context.log(issueComment);
-      return context.github.issues.createComment(issueComment);
-    }).catch(err => {
-      context.log('Failed' + err);
-      const issueComment = context.issue({ body: 'Failed to send payment' });
-      return context.github.issues.createComment(issueComment);
-    });
+  const amount = await getPrice(issue, repo);
+  return sendPayment(amount, issue, repo).then(res => {
+    console.log('Sent!');
+    const issueComment = context.issue({ body: `Sent a payment to ${issue.assignee.login} (${amount} €)` });
+    context.log(issueComment);
+    return context.github.issues.createComment(issueComment);
+  }).catch(err => {
+    context.log('Failed' + err);
+    const issueComment = context.issue({ body: 'Failed to send payment' });
+    return context.github.issues.createComment(issueComment);
+  });
 };
