b171196d6821b3cbfc213115d7b3c3e22b0b0768
Mikko Kurkela <mikko.kurkela@devolution.fi>
2020-07-22 11:06:24 +0300

    Log sendPayment payload

--- utils/sendPayment.js
+++ utils/sendPayment.js
@@ -5,15 +5,17 @@ module.exports = async (amount, issue, project) => {
     authorization: process.env.TASKER_APP_ID
   };
 
-  return axios.post(process.env.ROSTER_URL + '/postRecord',
-    {
+  const payload = {
       amount: amount,
       githubUser: issue.assignee.login,
       issue: issue.number,
       project: project,
       timestamp: Date.now(),
       description: issue.title
-    },
+    }
+  console.log()
+  return axios.post(process.env.ROSTER_URL + '/postRecord',
+    payload,
     {
       headers: headers
     });
